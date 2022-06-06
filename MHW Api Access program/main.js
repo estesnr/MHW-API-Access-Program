6bcfbdaa5c7d1bc7abe3de79820725f6aba739b4
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-06-05 20:41:56 -0400

    Add files via upload

---
+++ MHW Api Access program/main.js
@@ -0,0 +1,29 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function clearList(list) {
+    while(list.firstChild) {
+        list.firstChild.remove()
+    }
+}
+
+function getFetch() {
+    const choice = document.querySelector('input').value;
+    clearList(monstWeak)
+    let url = `https://mhwapi.herokuapp.com/api/${choice}`
+    fetch(url) 
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('#monstName').innerText = data.name
+            document.querySelector('#monstType').innerText = data.type
+            document.querySelector('#monstPic').src = data.img
+            document.querySelector('#weakTitle').innerText = 'WEAKNESSES'
+            data.weakness.forEach(el => {
+                console.log(el.element)
+                const li = document.createElement('li')
+                li.textContent = el.element
+                document.querySelector('#monstWeak').appendChild(li)
+            })
+        })
+}
+
